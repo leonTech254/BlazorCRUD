456893c9a301313a55025d0d617c913069a6afd9
leonTech254 <martinleontech23@gmail.com>
2024-01-05 00:08:35 +0300

    --add movies

---
+++ Models/MoviesModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorBasics.Models;
+
+public class MoviesModel
+{
+    public int id {  get; set; }
+    public string title { get; set; }
+    public String genre { get; set; }
+    public int Rating { get; set; }
+}
